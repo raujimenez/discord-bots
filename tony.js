4e27802104498f545eb75f2f29758643c6d2ffdb
Raul Jimenez <officialrauljimenez@gmail.com>
2020-03-17 20:58:02 -0500

    added formatting

--- tony.js
+++ tony.js
@@ -1,6 +1,7 @@
 const Discord = require("discord.js");
-const {Builder, By, Key, util} = require('selenium-webdriver');
-const fetch = require('node-fetch');
+const { Builder, By, Key, util } = require("selenium-webdriver");
+const { ServiceBuilder } = require("selenium-webdriver/chrome");
+const fetch = require("node-fetch");
 require("dotenv").config();
 
 const client = new Discord.Client();
@@ -13,39 +14,39 @@ client.on("ready", () => {
 
 client.on("message", async message => {
   if (/^!tony.status$/.test(message.content)) {
-    const response = await fetch('https://itsmetony.aternos.me/');
+    const response = await fetch("https://itsmetony.aternos.me/");
     const text = await response.text();
-    await message.reply(text.includes('offline') ? 'offline' : 'online')
-  }
-  else if (/^!tony.ip$/.test(message.content)) {
-    message.reply('`itsmetony.aternos.me`')
-  }
-  else if (/^!tony.start$/.test(message.content)) {
-    const driver = await new Builder().forBrowser('chrome').setChromeService('/usr/bin/chromedriver').build();
-    await driver.get('https://aternos.org/go/')
+    await message.reply(text.includes("offline") ? "offline" : "online");
+  } else if (/^!tony.ip$/.test(message.content)) {
+    message.reply("`itsmetony.aternos.me`");
+  } else if (/^!tony.start$/.test(message.content)) {
+    const driver = await new Builder()
+      .forBrowser("chrome")
+      .setChromeService(new ServiceBuilder("/usr/bin/chromedriver"))
+      .build();
+    await driver.get("https://aternos.org/go/");
     // login
-    await driver.findElement(By.id('user')).sendKeys('itsmetony')
-    await driver.findElement(By.id('password').sendKeys(process.env.aternoskey))
-    await driver.findElement(By.id('login')).click()
-    console.log('logged in');
+    await driver.findElement(By.id("user")).sendKeys("itsmetony");
+    await driver.findElement(
+      By.id("password").sendKeys(process.env.aternoskey)
+    );
+    await driver.findElement(By.id("login")).click();
+    console.log("logged in");
 
-    const start = await driver.findElement(By.id('start'))
+    const start = await driver.findElement(By.id("start"));
     if (start.isDisplayed()) {
-      start.click()
-      console.log('click');
+      start.click();
+      console.log("click");
     }
 
-    await message.reply('starting')
-
-  }
-  else if (/^!tony.help$/.test(message.content)) {
-    help = '!tony.up - test if server is up\n';
-    help += '!tony.ip - show ip address for server\n';
-    help += '!tony.start - start server';
-    help += '!tony.stop - stop server'; 
-    message.reply(help)
-  }
-  else if (/^!tony\.*$/.test(message.content)) {
-    message.reply('type !tony.help to see list of commands.')
+    await message.reply("starting");
+  } else if (/^!tony.help$/.test(message.content)) {
+    help = "!tony.up - test if server is up\n";
+    help += "!tony.ip - show ip address for server\n";
+    help += "!tony.start - start server";
+    help += "!tony.stop - stop server";
+    message.reply(help);
+  } else if (/^!tony\.*$/.test(message.content)) {
+    message.reply("type !tony.help to see list of commands.");
   }
 });
